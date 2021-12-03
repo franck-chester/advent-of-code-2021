ffae3a741bfb224f3566ee00569286ea520c95e5
franckchester <81856476+franck-chester@users.noreply.github.com>
2021-12-03 16:01:16 +0000

    refactored day03 part 2

--- day03/day03_02.js
+++ day03/day03_02.js
@@ -20,68 +20,53 @@ function bitsProcessor(s, bits) {
     }
 
     const newState = bits.map(process);
-
     console.log(`bitsProcessor(${JSON.stringify(s)}, ${JSON.stringify(bits)}) gives\n${JSON.stringify(newState)}`);
     return newState;
 };
 
-function msb(accumulator, state) {
-    accumulator.push(state.zero > state.one ? '0' : '1');
-    return accumulator;
+function calculateRate(processedLines, bitCalculator, description) {
+    let candidateEntries = processedLines;
+    const initialState = Array(processedLines.length).fill({
+        'zero': 0,
+        'one': 0
+    });
+
+    let bit = 0;
+
+    while (candidateEntries.length > 1 && bit < processedLines[0].length) {
+
+        console.log(`${description} iteration ${bit} -----------------------------------------------------`);
+        const finalState = candidateEntries.reduce(bitsProcessor, initialState);
+        const comparisonBitValue = bitCalculator(finalState, bit);
+        console.log(`finalState = ${JSON.stringify(finalState)}, finalState[${bit}] = ${finalState[bit]} : bit = ${comparisonBitValue}`);
+        candidateEntries = candidateEntries.filter(line => {
+            console.log(`${description} filter line ${JSON.stringify(line)} bit ${bit}: ${line[bit]} == ${comparisonBitValue} ?`);
+            return line[bit] == comparisonBitValue;
+        });
+        console.log(`-- ${candidateEntries.length} ${description} candidates matching bit  ${bit} == ${comparisonBitValue} `);
+        bit++;
+    }
+    console.log(`${description}  = ${candidateEntries[0].map(b => b ? 1 : 0)}  `);
+    const rating = parseInt(candidateEntries[0].map(b => b ? 1 : 0).join(''), 2);
+    console.log(`${description}  = ${rating}  `);
+    return rating
 }
 
-function lsb(accumulator, state) {
-    accumulator.push(state.zero < state.one ? '0' : '1');
-    return accumulator;
+function epsilonBit(finalState, bit) {
+    return finalState[bit].zero > finalState[bit].one;
 }
 
+function gammaBit(finalState, bit) {
+    return finalState[bit].one >= finalState[bit].zero;
+}
 
 async function main() {
     try {
         const processedLines = await getLines.processLines(path.resolve(__dirname, input), lineParser());
-        const initialState = Array(processedLines.length).fill({
-            'zero': 0,
-            'one': 0
-        });
-        
-        let oxygenRatingCandidates = processedLines;
-        let bit = 0;
-        while(oxygenRatingCandidates.length > 1 && bit < processedLines[0].length){
-            console.log(`-----------------------------------------------------`);
-            const finalState = oxygenRatingCandidates.reduce(bitsProcessor, initialState);
-            const gammaBit = finalState[bit].one >=  finalState[bit].zero;
-            console.log(`finalState = ${JSON.stringify(finalState )}, finalState[${bit}] = ${finalState[bit]} : gammaBit = ${gammaBit}`);
-            oxygenRatingCandidates = oxygenRatingCandidates.filter((line, row, lines) => {
-                console.log(`oxygenRatingCandidates filter line ${JSON.stringify(line)} bit ${bit}: ${line[bit]} == ${gammaBit } ?`)
-                return line[bit] == gammaBit ;
-            });
-            console.log(`-- ${oxygenRatingCandidates.length} oxygenRatingCandidates matching bit  ${bit} == ${gammaBit } `);
-            bit++;
-        }
-        console.log(`oxygenRating = ${oxygenRatingCandidates[0].map(b => b?1:0)}  `);
-        const oxygenRating= parseInt(oxygenRatingCandidates[0].map(b => b?1:0).join(''),2);
-        console.log(`oxygenRating = ${oxygenRating}  `);
+        const oxygenRating = calculateRate(processedLines, gammaBit, 'oxygenRating');
+        const co2Rating = calculateRate(processedLines, epsilonBit, 'co2Rating');
 
-
-        let co2RatingCandidates = processedLines;
-        bit = 0;
-        while(co2RatingCandidates.length > 1 && bit < processedLines[0].length){
-            console.log(`+++++++++++++++++++++++++++++++++++++++++++++++++`);
-            const finalState = co2RatingCandidates.reduce(bitsProcessor, initialState);
-            const epsilonBit = finalState[bit].zero > finalState[bit].one;
-            console.log(`finalState = ${JSON.stringify(finalState )}, finalState[${bit}] = ${JSON.stringify(finalState[bit])} : epsilonBit = ${epsilonBit}`);
-            co2RatingCandidates = co2RatingCandidates.filter((line, row, lines) => {
-                console.log(`co2RatingCandidates filter line ${JSON.stringify(line)} bit ${bit}: ${line[bit]} == ${epsilonBit } ?`)
-                return line[bit] == epsilonBit ;
-            });
-            console.log(`++ ${co2RatingCandidates.length} co2RatingCandidates matching bit  ${bit} == ${epsilonBit } `);
-            bit++;
-        }
-        console.log(`co2Rating = ${co2RatingCandidates[0].map(b => b?1:0)}  `);
-        const co2Rating= parseInt(co2RatingCandidates[0].map(b => b?1:0).join(''),2);
-        console.log(`co2Rating = ${co2Rating}  `);
-
-        console.log(`Solution  = ${ oxygenRating * co2Rating}  `);
+        console.log(`Solution  = ${oxygenRating * co2Rating}  `);
     }
     catch (e) {
         console.error(e);
@@ -89,4 +74,6 @@ async function main() {
 }
 
 main();
+
+
 
