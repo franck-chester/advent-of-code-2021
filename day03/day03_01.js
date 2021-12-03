0edaec02d246b7573de5c0c82373dcaaf5002259
franckchester <81856476+franck-chester@users.noreply.github.com>
2021-12-03 07:05:10 +0000

    day03 part 1

--- day03/day03_01.js
+++ day03/day03_01.js
@@ -1,8 +1,57 @@
 const getLines = require('../lib/getLines')
 const path = require('path')
 
-const input = './test.txt';
+//const input = './test.txt';
 const input = './input.txt';
 
-let entries = getLines.getIntegerLines(path.resolve(__dirname, input)); 
-let entries = getLines.getTextLines(path.resolve(__dirname, input)); 
+function lineParser() {
+    return (line) => {
+        return line.split('').map(c => c === '1');
+    };
+};
+
+function bitsProcessor(s, bits) {
+    console.log(`bitsProcessor(${JSON.stringify(s)}, ${JSON.stringify(bits)})...`);
+    const process = (f, i) => {
+        return {
+            'zero': s[i].zero + (f ? 0 : 1),
+            'one': s[i].one + (f ? 1 : 0)
+        }
+    }
+
+    const newState = bits.map(process);
+
+    console.log(`bitsProcessor(${JSON.stringify(s)}, ${JSON.stringify(bits)}) gives\n${JSON.stringify(newState)}`);
+    return newState;
+};
+
+function msb(accumulator, state) {
+    return accumulator + (state.zero > state.one ? '0' : '1');
+}
+
+function lsb(accumulator, state) {
+    return accumulator + (state.zero < state.one ? '0' : '1');
+}
+
+
+async function main() {
+    try {
+        const processedLines = await getLines.processLines(path.resolve(__dirname, input), lineParser());
+        const initialState = Array(processedLines.length).fill({
+            'zero': 0,
+            'one': 0
+        });
+        
+        const finalState = processedLines.reduce(bitsProcessor, initialState);
+        const gamma = parseInt(finalState.reduce(msb, ''),2);
+        const epsilon = parseInt(finalState.reduce(lsb, ''),2);
+        console.log(`Gamma : ${gamma}, Epsilon : ${epsilon}`);
+        console.log(`Solution : ${gamma*epsilon}`);
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+
+main();
+
