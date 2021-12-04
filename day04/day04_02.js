7eec2001dc63a16ae800b5cf41978a8ffeeee281
franckchester <81856476+franck-chester@users.noreply.github.com>
2021-12-04 16:41:53 +0000

    day04 part 2 complete (big mess)

--- day04/day04_02.js
+++ day04/day04_02.js
@@ -1,8 +1,133 @@
-const getLines = require('../lib/getLines')
 const path = require('path')
+let fs = require('fs');
 
-const input = './test.txt';
+//const input = './test.txt';
 const input = './input.txt';
 
-let entries = getLines.getIntegerLines(path.resolve(__dirname, input)); 
-let entries = getLines.getTextLines(path.resolve(__dirname, input)); 
+
+function getLines(inputFile) {
+    const data = fs.readFileSync(inputFile, 'utf8');
+    return data.split(/\r?\n/);
+}
+
+
+async function main() {
+    try {
+        const puzzleInput = getLines(path.resolve(__dirname, input));
+        const bingoCall = puzzleInput[0].split(',');
+        const numbersToBoard = new Map();
+        console.log(`Bingo call : ${JSON.stringify(bingoCall)}`);
+
+        const boards = [];
+        let rows = [];
+        let cols = [[], [], [], [], []];
+        let row = 0;
+        let boardIndex = 0;
+        let sum = 0;
+        for (line = 2; line < puzzleInput.length; line++) {
+
+            numbersInRow = puzzleInput[line].match(/\d+/g);
+            rows.push(numbersInRow);
+            col = 0;
+            for (n of numbersInRow) {
+                //console.log(`add ${n} to ${sum}`)
+                sum += parseInt(n);
+                cols[col].push(n);
+                if (!numbersToBoard.has(n)) {
+                    numbersToBoard.set(n, new Set());
+                }
+                let matchingBoards = numbersToBoard.get(n);
+                matchingBoards.add(boardIndex);
+                numbersToBoard.set(n, matchingBoards);
+                col++;
+            }
+            row++;
+            if (row == 5) {
+                boards.push({
+                    rows: rows,
+                    cols: cols,
+                    rowsCount: [0, 0, 0, 0, 0],
+                    colsCount: [0, 0, 0, 0, 0],
+                    sum: sum,
+                    originalSum: sum,
+                    matched: [],
+                    bingo: false
+                });
+                rows = [];
+                cols = [[], [], [], [], []];
+                line++; // jump empty line
+                row = 0;
+                sum = 0;
+                boardIndex++;
+            }
+        }
+        console.log(`Loaded ${boardIndex} boards:\n${JSON.stringify(boards, null, 2)}`);
+        //console.log(`numbersToBoard: ${JSON.stringify([...numbersToBoard], null, 2)}`);
+
+        for (call of bingoCall) {
+            console.log(`Calling... ${call}...!!`);
+            if (!numbersToBoard.has(call)) {
+                console.log(`No board has ${call}`);
+                continue;
+            }
+            let matchingBoards = Array.from(numbersToBoard.get(call));
+
+            console.log(`** Found ${matchingBoards.length} matchingBoards for ${call}: ${JSON.stringify(matchingBoards)}`);
+            let bingo = false;
+
+            for (boardId of matchingBoards) {
+                bingo = false;
+                let matchingBoard = boards[boardId];
+                if (matchingBoard.bingo) {
+                    continue;
+                }
+                //console.log(`BEFORE matchingBoard ${boardId} for ${call}:\n${JSON.stringify(matchingBoard)}`);
+
+                for (row of matchingBoard.rows) {
+                    matchIndex = row.indexOf(call);
+                    if (matchIndex > -1) {
+                        matchingBoard.colsCount[matchIndex]++;
+                    }
+                }//for (row of matchingBoard.rows)
+                for (col of matchingBoard.cols) {
+                    matchIndex = col.indexOf(call);
+                    if (matchIndex > -1) {
+                        matchingBoard.rowsCount[matchIndex]++;
+                    }
+                }
+
+                matchingBoard.sum = matchingBoard.sum - parseInt(call);
+                matchingBoard.matched.push(call);
+                if (matchingBoard.rowsCount.includes(5) || matchingBoard.colsCount.includes(5)) {
+                    console.log(`BINGO!!!!`);
+                    bingo = true;
+                    matchingBoard.bingo = true;
+                    console.log(`MatchingBoard ${boardId} for ${call}:\n${JSON.stringify(boards[boardId])}`);
+                    let sum = boards[boardId].matched.reduce(function (a, b) {
+                        return a + parseInt(b);
+                    }, 0);
+                    console.log(`sum of board = ${boards[boardId].originalSum} - sum of matches = ${sum} = ${boards[boardId].originalSum - sum}`);
+                    console.log(`Solution = ${call} * ${boards[boardId].sum} = ${call * boards[boardId].sum}`);
+                }
+
+                console.log(`AFTER matchingBoard ${boardId} for ${call}:\n${JSON.stringify(matchingBoard)}`);
+
+            }//for (boardId of matchingBoards) 
+            if (bingo) {
+                console.log(`BINGO AFTER matchingBoard ${boardId} for ${call} of ${JSON.stringify(boards[boardId].matched)}`);
+                console.log(`MatchingBoard ${boardId} for ${call}:\n${JSON.stringify(boards[boardId])}`);
+                let sum = boards[boardId].matched.reduce(function (a, b) {
+                    return a + parseInt(b);
+                }, 0);
+                console.log(`sum of board = ${boards[boardId].originalSum} - sum of matches = ${sum} = ${boards[boardId].originalSum - sum}`);
+                console.log(`Solution = ${call} * ${boards[boardId].sum} = ${call * boards[boardId].sum}`);
+
+            }
+        }//for (call of bingoCall) 
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+
+main();
