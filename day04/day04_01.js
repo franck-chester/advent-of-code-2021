3e9358c9437d6e13c831eeff375012686da46763
franckchester <81856476+franck-chester@users.noreply.github.com>
2021-12-04 06:17:31 +0000

    reading boards OK

--- day04/day04_01.js
+++ day04/day04_01.js
@@ -1,31 +1,36 @@
-const getLines = require('../lib/getLines')
 const path = require('path')
+let fs = require('fs');
 
 const input = './test.txt';
 //const input = './input.txt';
 
 
-function lineParser() {
-    return (line) => {
-        return line; // TODO implement parser
-    };
-};
-
-function entryProcessor(previousState, entry) {
-    console.log(`entryProcessor(${JSON.stringify(previousState)}, ${JSON.stringify(entry)})...`);
-    const newState = {};
-
-    console.log(`bitsProcessor(${JSON.stringify(previousState)}, ${JSON.stringify(entry)}) gives\n${JSON.stringify(newState)}`);
-    return newState;
-};
+function getLines(inputFile) {
+    const data = fs.readFileSync(inputFile, 'utf8');
+    return data.split(/\r?\n/);
+}
 
 
 async function main() {
     try {
-        const entries = await getLines.processLines(path.resolve(__dirname, input), lineParser());     
-        const initialState = {}  
-        const finalState = entries.reduce(entryProcessor, initialState);
-        console.log(`main(): ${JSON.stringify(finalState)}`);
+        const puzzleInput = getLines(path.resolve(__dirname, input));   
+        const bingoCall = puzzleInput[0].split(',');
+        console.log(`Bingo call : ${JSON.stringify(bingoCall)}`);
+        
+        const boards = [];
+        let board = [];
+        let row=0;
+        for(line = 2; line<puzzleInput.length; line++){
+            board.push(puzzleInput[line].match(/\d+/g));
+            row++;
+            if(row==5){
+                boards.push(board);
+                board = [];
+                line++; // jump empty line
+                row = 0;
+            }
+        }
+        console.log(`main(): ${JSON.stringify(boards, null, 2)}`);
     }
     catch (e) {
         console.error(e);
