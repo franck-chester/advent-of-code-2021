02cd01da6fe85437b068c62a416915d7b9bf4436
franckchester <81856476+franck-chester@users.noreply.github.com>
2021-12-10 11:11:45 +0000

    day10 part 2

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # advent-of-code-2021
-My 2021 attempt at the https://adventofcode.com/ challenge
+My 2021 attempt at the https://adventofcode.com/ challenge.
+
+Started writing the following on day 10, hence the lack of details :-)
+
+## Day 1: Sonar Sweep
+
+Easy one
+
+## Day 2: Dive!
+
+Easy one -= but decided it was worth my while to use map / reduce /filter
+
+## Day 3: Binary Diagnostic
+
+Easy one.
+
+## Day 4: Giant Squid
+
+Not a very smart implementation, but works.
+Part 2 is just part 1 with a few breaks removed to count till the end rather than stop at the first winning board
+
+## Day 5: Hydrothermal Venture
+
+Easy one, once I stopped kidding myself there was some secret math to count intersections;
+
+## Day 6: Lanternfish
+
+Part 1 was easy to achieve by tracking the cycle date per fish,
+
+Part 2 is technically speaking the same than part 1, but the factorial blows the basic implementation out of the water, run out of memory, too many fish!!
+
+I really struggled for a while, really thought there was some smart math at play.
+
+In the end the trick was to stop tracking the cycle date per fish, and start tracking the number of fish at any date of the cycle.
+
+## Day 7: The Treachery of Whales
+
+Part 1 I wasted a huge amount of time looking for smart math to solve it.
+Even looked into [KD Trees](https://en.wikipedia.org/wiki/K-d_tree)!!!
+
+In the end, wrote the basic code and got there :-)
+
+Part 2 is not much more work after that, as long as you remember the [triangle number formula](https://www.mathsisfun.com/algebra/triangular-numbers.html)
+## Day 8: Seven Segment Search
+
+Part 1 is a slog but simple enough.
+Suspect there is a smart binary math method cleaner than all the switch statements, but beyond me I'm afraid
+
+Part 2 took me a while.
+It's a bit like sudoku, figure our which order to eliminate possibilities
+
+## Day 9: Smoke Basin
+
+First part is simple enough.
+Second part I haven't done - got a cold and just couldn't think.
+Main issue that worries me is connecting basin... I can see how to mark a cell as part of a basin (surrounded by cells of different height, not 9), but identifying th basin is beyond me
+## Day 10: Syntax Scoring
+
+An easy one - but I have a cold and won't refactor my very basic implementation
--- day10/day10_02.js
+++ day10/day10_02.js
@@ -1,31 +1,94 @@
 const getLines = require('../lib/getLines')
 const path = require('path')
 
-const input = './test.txt';
-//const input = './input.txt';
+//const input = './test.txt';
+const input = './input.txt';
 
 
 function lineParser() {
     return (line) => {
-        return line; // TODO implement parser
+        return line.split('');
     };
 };
 
-function entryProcessor(previousState, entry) {
-    console.log(`entryProcessor(${JSON.stringify(previousState)}, ${JSON.stringify(entry)})...`);
-    const newState = {};
+function isCorrupt(entry) {
+    const open = [];
+    let corrupt = false;
+    let errorScore = 0;
+    matchingPairs = new Map([['(', ')'], ['[', ']'], ['{', '}'], ['<', '>']])
+    index = 0;
+    for (bracket of entry) {
+        if (matchingPairs.has(bracket)) {
+            open.push(bracket);
+        }
+        else {
+            const matchingOpening = open.pop();
+            const expecting = matchingPairs.get(matchingOpening);
+            corrupt = (bracket != expecting)
 
-    console.log(`bitsProcessor(${JSON.stringify(previousState)}, ${JSON.stringify(entry)}) gives\n${JSON.stringify(newState)}`);
-    return newState;
+            if (corrupt) {
+                console.log(`${entry.join('')} : expected ${matchingOpening}, found ${bracket} at index ${index} - discarding`);
+                return true;
+            }
+        }
+        index++;
+    }
+
+    return false
+}
+
+function calculateScore(entry) {
+    const open = [];
+    matchingPairs = new Map([['(', ')'], ['[', ']'], ['{', '}'], ['<', '>']])
+
+    for (bracket of entry) {
+        if (matchingPairs.has(bracket)) {
+            open.push(bracket);
+        }
+        else {
+            open.pop();
+        }
+    }
+
+    // now start completing
+    let score = 0
+    console.log(`${entry.join('')} : opened brackets are : ${open.join('')}`);
+    while (openingBracket = open.pop()) {
+        
+        score *= 5;
+        switch (openingBracket) {
+            case '(':
+                score += 1;
+                break;
+            case '[':
+                score += 2;
+                break;
+            case '{':
+                score += 3;
+                break;
+            case '<':
+                score += 4;
+                break;
+        }
+        console.log(`--> ${open.join('')} popped ${openingBracket} - score = ${score}`);
+    }
+    console.log(`--> ${entry.join('')} : score = ${score}`);
+
+    return score;
 };
 
 
 async function main() {
     try {
-        const entries = await getLines.processLines(path.resolve(__dirname, input), lineParser());     
-        const initialState = {}  
-        const finalState = entries.reduce(entryProcessor, initialState);
-        console.log(`main(): ${JSON.stringify(finalState)}`);
+        const rawEntries = await getLines.processLines(path.resolve(__dirname, input), lineParser());
+        const entries = rawEntries.filter(line => !isCorrupt(line));
+        console.log('-----------------------------------------------------------------');
+        console.log(`${entries.length} remaining after discarding corrupt ones`);
+        const completionScores = entries.map(calculateScore, 0);
+        completionScores.sort((a,b)=>a-b);
+        console.log(`Completion scores: ${completionScores}`);
+        const middleScore = completionScores[(completionScores.length-1)/2];
+        console.log(`Middle completeion score: ${middleScore}`);
     }
     catch (e) {
         console.error(e);
