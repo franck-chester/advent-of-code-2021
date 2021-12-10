c3ed66123c4da8ee0923a3667744d0da3602139a
franckchester <81856476+franck-chester@users.noreply.github.com>
2021-12-10 06:55:13 +0000

    day 09 WIP!!!

--- day09/day09_02.js
+++ day09/day09_02.js
@@ -7,25 +7,43 @@ const input = './test.txt';
 
 function lineParser() {
     return (line) => {
-        return line; // TODO implement parser
+        return line.split('').map(c => parseInt(c))
     };
 };
 
-function entryProcessor(previousState, entry) {
-    console.log(`entryProcessor(${JSON.stringify(previousState)}, ${JSON.stringify(entry)})...`);
-    const newState = {};
-
-    console.log(`bitsProcessor(${JSON.stringify(previousState)}, ${JSON.stringify(entry)}) gives\n${JSON.stringify(newState)}`);
-    return newState;
-};
-
-
 async function main() {
     try {
-        const entries = await getLines.processLines(path.resolve(__dirname, input), lineParser());     
-        const initialState = {}  
-        const finalState = entries.reduce(entryProcessor, initialState);
-        console.log(`main(): ${JSON.stringify(finalState)}`);
+        // entries is an array of rows of heights
+        const entries = await getLines.processLines(path.resolve(__dirname, input), lineParser());
+        // risks is an array of rows of risks 
+        const cmax = entries[0].length - 1;
+        const rmax = entries.length - 1
+        const risks = [[]]
+        const basins = [[]]
+        let totalRisk = 0;
+        for (let r = 0; r <= rmax; r++) {
+            for (let c = 0; c <= cmax; c++) {
+                // part of basin if any directly adjacent (no diagonal) cell is a different height, but not 9
+                let thisHeight = entries[r][c];
+                if (thisHeight == 9) {
+                    continue;
+                }
+                let isPartOfBasin = false;
+                isPartOfBasin = isPartOfBasin || (r > 0 && entries[r - 1][c] != thisHeight);
+                isPartOfBasin = isPartOfBasin || (r < rmax - 1 0 && entries[r + 1][c] != thisHeight);
+                isPartOfBasin = isPartOfBasin || (c > 0 && entries[r][c - 1] != thisHeight);
+                isPartOfBasin = isPartOfBasin || (rc < cmax - 1 0 && entries[r][c + 1] != thisHeight);
+
+                risks[r].push(isLowPoint ? 1 + entries[r][c] : 0);
+                console.log('--')
+            }
+            risks.push([])
+            console.log('------')
+        }
+        console.log(`Solution : ${totalRisk}`)
+        // for (let r = 0; r <= rmax; r++) {
+        //     console.log(risks[r].join(''))
+        // }
     }
     catch (e) {
         console.error(e);
